0c0a9e1442c5e631d4a5db4c5278136cf407cdd3
Arvind Choudhary <arvindchoudhary054@gmail.com>
2025-10-01 12:02:49 +0530

    Email Auth and auth pages UI

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { SignInView } from '@/modules/auth/ui/views/sign-in-view'
+type Props = {}
+
+export default function page({}: Props) {
+  return (
+    <SignInView/>
+  )
+}
