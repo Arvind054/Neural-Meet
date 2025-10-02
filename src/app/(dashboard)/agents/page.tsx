8761996200bd82bad5102848fd043c26b5d164e4
Arvind Choudhary <arvindchoudhary054@gmail.com>
2025-10-02 23:21:55 +0530

    Added Agent Form and Procedures

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,12 +1,25 @@
+import { auth } from "@/lib/auth";
+import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
 import { AgentsView, AgentsViewError, AgentsViewLaoding } from "@/modules/agents/ui/views/agents-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import {ErrorBoundary} from 'react-error-boundary';
 const page = async()=>{
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+     const session = await  auth.api.getSession({
+        headers: await headers(),
+      })
+      if(!session){
+        redirect("/sign-in");
+      }
     return (
+        <>
+        <AgentsListHeader/>
         <HydrationBoundary state={dehydrate(queryClient)}>
             <Suspense fallback = {<AgentsViewLaoding/>}>
             <ErrorBoundary fallback={<AgentsViewError/>}>
@@ -14,6 +27,7 @@ const page = async()=>{
         </ErrorBoundary>
         </Suspense>
         </HydrationBoundary>
+        </>
     )
 }
 
--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -1,10 +1,29 @@
 import { db } from "@/DB";
 import { agents } from "@/DB/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import { createTRPCRouter, baseProcedure, protectedProcedure } from "@/trpc/init";
+import { agentsInsertSchema } from "../schemas";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-    getMany: baseProcedure.query(async()=>{
+    // Get one
+    getOne: protectedProcedure.input(z.object({id:z.string()})).query(async({input})=>{
+        const [existingAgent] = await db.select().from(agents).where(eq(agents.id, input.id));
+        return existingAgent;
+    }),
+    //Get Many
+    getMany: protectedProcedure.query(async()=>{
         const data = await db.select().from(agents);
         return data;
     }),
+    create: protectedProcedure
+    .input(agentsInsertSchema)
+    .mutation(async({input, ctx})=>{
+      const [createdAgent] = await db.insert(agents)
+      .values({
+        ...input,
+        userId: ctx.auth.user.id
+      }).returning();
+      return createdAgent;
+    })
 })
