43f2d158c1e801633922504ffc133b95da173718
Arvind Choudhary <arvindchoudhary054@gmail.com>
2025-10-03 23:18:27 +0530

    Agent Pagination and Pagination client and Server Filter synchronisation

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/lib/auth";
+import { loadSearchParams } from "@/modules/agents/params";
 import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
 import { AgentsView, AgentsViewError, AgentsViewLaoding } from "@/modules/agents/ui/views/agents-view";
 import { getQueryClient, trpc } from "@/trpc/server";
@@ -6,11 +7,17 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { SearchParams } from "nuqs";
 import { Suspense } from "react";
 import {ErrorBoundary} from 'react-error-boundary';
-const page = async()=>{
+
+interface Props{
+  searchParams: Promise<SearchParams>;
+};
+const page = async({searchParams}:Props)=>{
+  const filters = await loadSearchParams(searchParams);
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({...filters}));
      const session = await  auth.api.getSession({
         headers: await headers(),
       })
