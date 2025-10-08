7f0ec78885f25d8d50474bab765c0c3f4a156ddc
Arvind Choudhary <arvindchoudhary054@gmail.com>
2025-10-08 22:45:03 +0530

    Video Call (stream Integration)

--- src/modules/meetings/server/procedure.ts
+++ src/modules/meetings/server/procedure.ts
@@ -7,7 +7,10 @@ import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@
 import { TRPCError } from "@trpc/server";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
 import { MeetingStatus } from "../types";
+import { streamVideo } from "@/lib/stream-video";
+import { generateAvatarUri } from "@/lib/avatar";
 export const meetingsRouter = createTRPCRouter({
+   
     // Get one
     getOne: protectedProcedure.input(z.object({id:z.string()})).query(async({input, ctx})=>{
         const [existingMeeting] = await db.select({
@@ -60,6 +63,7 @@ export const meetingsRouter = createTRPCRouter({
         }
         
     }),
+  // Create new Meeting
      create: protectedProcedure
         .input(meetingsInsertSchema)
         .mutation(async({input, ctx})=>{
@@ -68,8 +72,46 @@ export const meetingsRouter = createTRPCRouter({
             ...input,
             userId: ctx.auth.user.id
           }).returning();
+          const call = streamVideo.video.call('default', createdMeeting.id);
+          await call.create({
+            data : {
+              created_by_id : ctx.auth.user.id,
+              custom:{
+                meetingId: createdMeeting.id,
+                meetingName: createdMeeting.name
+              },
+              settings_override:{
+                transcription:{
+                  language: "en",
+                  mode: 'auto-on',
+                  closed_caption_mode:"auto-on"
+                },
+                recording:{
+                  mode: 'auto-on',
+                  quality:'1080p'
+                },
+              }
+            }
+          });
+          const [existingAgent] = await db.select().from(agents).where(eq(agents.id, createdMeeting.agentId));
+          if(!existingAgent){
+            throw new TRPCError({
+              code: "NOT_FOUND",
+              message: "Agent Not Found",
+            });
+          }
+          await streamVideo.upsertUsers([
+            {
+              id: existingAgent.id,
+              name: existingAgent.name,
+              role: 'user',
+              image: generateAvatarUri({seed: existingAgent.name, variant : 'botttsNeutral'})
+            }
+          ])
           return createdMeeting;
         }),
+
+      // To Update the Meeting Details
     update: protectedProcedure.input(meetingsUpdateSchema)
         .mutation(async({ctx, input})=>{
             const [updateMeeting] = await db.update(meetings).set(input).where(and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))).returning();
@@ -78,6 +120,8 @@ export const meetingsRouter = createTRPCRouter({
             }
             return updateMeeting;
         }),
+
+      // TO Remove the Meeting 
     remove: protectedProcedure.input(z.object({id: z.string()}))
         .mutation(async({ctx, input})=>{
             const [removedMeeting] = await db.delete(meetings)
@@ -86,5 +130,26 @@ export const meetingsRouter = createTRPCRouter({
               throw new TRPCError({code: "NOT_FOUND", message :"Meeting Not Found"})
             }
             return removedMeeting;
+        }),
+
+        //Generate Token
+        generateToken :  protectedProcedure.mutation(async({ctx})=>{
+           await streamVideo.upsertUsers([
+            {
+              id: ctx.auth.user.id,
+              name: ctx.auth.user.name,
+              role: "admin",
+              image: ctx.auth.user.image ?? generateAvatarUri({seed: ctx.auth.user.name, variant: 'initials'})
+            }
+           ]);
+
+           const expirationTime = Math.floor(Date.now()/1000) + 3600; // 1 Hour Time
+           const issuedAt = Math.floor(Date.now()/1000)-60;
+           const token = streamVideo.generateUserToken({
+            user_id: ctx.auth.user.id,
+            exp: expirationTime,
+            validity_in_seconds: issuedAt,
+           });
+           return token;
         }),
 })
